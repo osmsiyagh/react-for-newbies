16e94feed9c2f5ddd899a39989e244c8d9b757fb
osmsiyagh <o.lions.siyagh@gmail.com>
2020-08-13 04:05:55 +0100

    filter fonction

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
 import React from 'react';
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-};
-function App() {
-  return (
-  <div>
-    <h1>
-      {welcome.greeting} {welcome.title}
-    </h1>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-  </div>
-  );
-}
-export default App;
 
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter(story =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <Search onSearch={handleSearch} />
+
+      <hr />
+
+      <List list={searchedStories} />
+    </div>
+  );
+};
+
+const Search = props => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={props.onSearch} />
+  </div>
+);
+
+const List = props =>
+  props.list.map(item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
+
+export default App;
